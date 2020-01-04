29c893485d427e22c9e54115882ff80c8ac98538
fonkamloic <fonkamloic@gmail.com>
2020-01-05 00:59:45 +0100

    compose button component

--- lib/composeButton.dart
+++ lib/composeButton.dart
@@ -1,25 +1,34 @@
+import 'package:email_app/message.dart';
 import 'package:email_app/message_compose.dart';
 import 'package:flutter/material.dart';
 
 class ComposeButton extends StatelessWidget {
+  final List<Message> messages;
+  ComposeButton({this.messages});
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
       child: Icon(Icons.add),
       onPressed: () async {
-        String intention = await Navigator.push(
+        Message message = await Navigator.push(
           context,
           MaterialPageRoute(
             builder: (context) => MessageCompose(),
           ),
         );
-        Scaffold.of(context).showSnackBar(
-          SnackBar(
-            content: Text("Your message was sent with $intention"),
-            backgroundColor: intention == "Love" ? Colors.greenAccent : Colors.redAccent,
-            duration: Duration(seconds: 1),
-          ),
-        );
+
+        if(message != null){
+          messages.add(message);
+          Scaffold.of(context).showSnackBar(
+            SnackBar(
+              content: Text("Your message was been sent"),
+              backgroundColor: Colors.greenAccent,
+              duration: Duration(seconds: 1),
+            ),
+          );
+        }
+
+
       },
     );
   }
