59d686bb21be6d9acbe8c6f6022be4e516d42102
fonkamloic <fonkamloic@gmail.com>
2020-01-06 01:58:25 +0100

    message class

--- lib/message.dart
+++ lib/message.dart
@@ -1,10 +1,9 @@
 import 'dart:convert';
 
-import 'package:json_annotation/json_annotation.dart';
 import 'package:http/http.dart' as http;
+import 'package:json_annotation/json_annotation.dart';
 
 part 'message.g.dart';
-
 
 @JsonSerializable()
 class Message {
@@ -13,18 +12,25 @@ class Message {
 
   Message({this.body, this.subject});
 
- factory  Message.fromJson(Map<String, dynamic> msg)  => _$MessageFromJson(msg);
+  factory Message.fromJson(Map<String, dynamic> msg) => _$MessageFromJson(msg);
+
 //    : subject = msg["subject"],
 //        body = msg["body"];
 
-  static Future<List<Message>> browse() async {
+  static Future<List<Message>> browse({status = "important"}) async {
     //String content = await rootBundle.loadString('data/messages.json');
-    http.Response response = await http.get("http://www.mocky.io/v2/5e10880d35000066001e696e");
+    String url = status == "important"
+        ? "http://www.mocky.io/v2/5e1527dd2d000060851674e1"
+        : "http://www.mocky.io/v2/5e10880d35000066001e696e";
+    http.Response response = await http.get(url);
+
+    // http://www.mocky.io/v2/5e1527dd2d000060851674e1
+
     String content = response.body;
     List collection = json.decode(content);
-    List<Message>  _messages = collection.map((json) => Message.fromJson(json)).toList();
+    List<Message> _messages =
+        collection.map((json) => Message.fromJson(json)).toList();
 
     return _messages;
-
   }
 }
