f6dda84ec47b837454f09821353b9f6e2bf11074
fonkamloic <fonkamloic@gmail.com>
2020-01-07 23:01:51 +0100

    message class

--- lib/message.dart
+++ lib/message.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:json_annotation/json_annotation.dart';
+import 'package:http/http.dart' as http;
 
 part 'message.g.dart';
 
@@ -14,5 +17,14 @@ class Message {
 //    : subject = msg["subject"],
 //        body = msg["body"];
 
+  static Future<List<Message>> browse() async {
+    //String content = await rootBundle.loadString('data/messages.json');
+    http.Response response = await http.get("http://www.mocky.io/v2/5e10880d35000066001e696e");
+    String content = response.body;
+    List collection = json.decode(content);
+    List<Message>  _messages = collection.map((json) => Message.fromJson(json)).toList();
 
+    return _messages;
+
+  }
 }
