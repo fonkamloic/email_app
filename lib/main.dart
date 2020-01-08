f0b0b261538db40d59cfda6c1db0b7584ec901dc
fonkamloic <fonkamloic@gmail.com>
2020-01-08 15:56:29 +0100

    main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:email_app/App.dart';
+import 'package:email_app/overseer.dart';
+import 'package:email_app/provider.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(emailApp());
@@ -7,13 +9,16 @@ class emailApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo, accentColor: Colors.indigoAccent
+    return Provider(
+      data: OverSeer(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+            primarySwatch: Colors.indigo, accentColor: Colors.indigoAccent),
+        home: App(
+          title: "Email App",
+        ),
       ),
-      home: App(title: "Email App",),
     );
   }
 }
-
