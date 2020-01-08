90e195623a959d20c4bcac9f43f8e60c1b3b7e7b
fonkamloic <fonkamloic@gmail.com>
2020-01-08 22:20:27 +0100

    counter component

---
+++ lib/counter.dart
@@ -0,0 +1,19 @@
+import 'package:email_app/counterManager.dart';
+import 'package:email_app/observer.dart';
+import 'package:email_app/provider.dart';
+import 'package:flutter/material.dart';
+
+class Counter extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    CounterManager manager = Provider.of(context).fetch(CounterManager);
+    return Center(
+      child: Observer<int>(
+        stream: manager.counter$,
+        onSuccess: (context, data) {
+          return Text("Calendar: $data");
+        },
+      ),
+    );
+  }
+}
