93551bdd4f2a511bcf33b9595487fe3c6e4eef9c
fonkamloic <fonkamloic@gmail.com>
2020-01-08 15:56:25 +0100

    contact screen

--- lib/contactScreen.dart
+++ lib/contactScreen.dart
@@ -1,58 +1,51 @@
 import 'package:email_app/AppDrawer.dart';
+import 'package:email_app/contactCounter.dart';
 import 'package:email_app/contactListBuilder.dart';
-import 'package:email_app/contactManager.dart';
-import 'package:email_app/contact_search_delegate.dart';
+import 'package:email_app/contact_search.dart';
 import 'package:email_app/model/contact.dart';
+import 'package:email_app/overseer.dart';
+import 'package:email_app/provider.dart';
 import 'package:flutter/material.dart';
 
+import 'contactManager.dart';
+
 class ContactScreen extends StatelessWidget {
-  ContactManager manager = ContactManager();
 
   @override
   Widget build(BuildContext context) {
+    ContactManager manager = Provider.of(context).fetch(ContactManager);
+
     return DefaultTabController(
       length: 2,
       child: Scaffold(
         appBar: AppBar(
           title: Text("Contacts"),
           actions: <Widget>[
-            Chip(
-              label: StreamBuilder<int>(
-                  stream: manager.contactCounter,
-                  builder: (context, snapshot) {
-                    return Text(
-                      (snapshot.data ?? 0).toString(),
-                      style: TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.bold),
-                    );
-                  }),
-              backgroundColor: Colors.red,
-            ),
+            ContactCounter(),
             IconButton(
               icon: Icon(Icons.search),
               onPressed: () {
-                showSearch(
-                    context: context,
-                    delegate: ContactSearchDelegate(manager: manager));
+                showSearch(context: context, delegate: ContactSearch());
               },
             ),
           ],
         ),
         drawer: AppDrawer(),
         body: ContactListBuilder(
-          stream: manager.contactListNow,
-          builder: (context, contact) => ListView.separated(
-            itemCount: contact.length,
-            itemBuilder: (BuildContext context, int index) {
-              Contact _contact = contact[index];
-              return ListTile(
-                leading: CircleAvatar(),
-                title: Text(_contact.name),
-                subtitle: Text(_contact.email),
-              );
-            },
-            separatorBuilder: (context, index) => Divider(),
-          ),
+          stream:manager.contactListNow,
+          builder: (context, contact) =>
+              ListView.separated(
+                itemCount: contact.length,
+                itemBuilder: (BuildContext context, int index) {
+                  Contact _contact = contact[index];
+                  return ListTile(
+                    leading: CircleAvatar(),
+                    title: Text(_contact.name),
+                    subtitle: Text(_contact.email),
+                  );
+                },
+                separatorBuilder: (context, index) => Divider(),
+              ),
         ),
       ),
     );
