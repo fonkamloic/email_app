ce91e98f539ef9dff6b2193e20a9812e6c871592
fonkamloic <fonkamloic@gmail.com>
2020-01-08 15:55:55 +0100

    contact manager

--- lib/contactManager.dart
+++ lib/contactManager.dart
@@ -2,27 +2,28 @@ import 'dart:async';
 
 import 'package:email_app/model/contact.dart';
 import 'package:email_app/services/contactService.dart';
+import 'package:rxdart/rxdart.dart';
 
 class ContactManager {
-  final StreamController<int> _countCounter = StreamController<int>();
+  final BehaviorSubject<int> _countCounter = BehaviorSubject<int>();
 
   Stream<int> get contactCounter => _countCounter.stream;
-  Stream<int>  filteredCounter({query}) => _countCounter.stream;
+
+  Stream<int> filteredCounter({query}) => _countCounter.stream;
 
   Stream<List<Contact>> get contactListNow async* {
     yield await ContactService.browse();
 
-
 //   Stream.fromFuture(ContactService.browse());
-   }
+  }
 
   Stream<List<Contact>> filteredContactList({query}) async* {
-   yield await ContactService.browse(query: query);
+    yield await ContactService.browse(query: query);
   }
-    //  Stream.fromFuture(ContactService.browse(query: query));
+
+  //  Stream.fromFuture(ContactService.browse(query: query));
 
   ContactManager() {
     contactListNow.listen((list) => _countCounter.add(list.length));
-    filteredContactList().listen((list)=> _countCounter.add(list.length));
   }
 }
