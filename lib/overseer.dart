08fa954455a8156280ad3cc3d2f254ffb03311c3
fonkamloic <fonkamloic@gmail.com>
2020-01-08 22:21:51 +0100

    providers manager

--- lib/overseer.dart
+++ lib/overseer.dart
@@ -1,10 +1,12 @@
 import 'package:email_app/contactManager.dart';
+import 'package:email_app/counterManager.dart';
 
 class OverSeer {
   Map<dynamic, dynamic> repository = {};
 
   OverSeer() {
     register(ContactManager, ContactManager());
+    register(CounterManager, CounterManager());
   }
 
   register(name, object) {
