8017b127de7bcfe92866bf58ca2907d06c095abd
fonkamloic <fonkamloic@gmail.com>
2020-01-09 04:25:11 +0100

    serverside search

--- lib/contact_search.dart
+++ lib/contact_search.dart
@@ -29,32 +29,32 @@ class ContactSearch extends SearchDelegate {
 
   @override
   Widget buildResults(BuildContext context) {
-    ContactManager manager = Provider.of(context).fetch(ContactManager);
-    manager.inFilter.add(query);
-    if (query.length < 3) {
-      return Center(
-        child: Text("Type at least 3 letters to search"),
-      );
-    } else
-      return ContactListBuilder(
-        stream: manager.browse$,
-        builder: (context, contact) => ListView.separated(
-          itemCount: contact.length,
-          itemBuilder: (BuildContext context, int index) {
-            Contact _contact = contact[index];
-            return ListTile(
-              leading: CircleAvatar(),
-              title: Text(_contact.name),
-              subtitle: Text(_contact.email),
-            );
-          },
-          separatorBuilder: (context, index) => Divider(),
-        ),
-      );
+    return Container();
   }
 
   @override
-  Widget buildSuggestions(BuildContext context) {
-    return Container();
+  Widget buildSuggestions(BuildContext context) { ContactManager manager = Provider.of(context).fetch(ContactManager);
+  manager.inFilter.add(query);
+//  if (query.length < 3) {
+//    return Center(
+//      child: Text("Type at least 3 letters to search"),
+//    );
+//  } else
+    return ContactListBuilder(
+      stream: manager.browse$,
+      builder: (context, contact) => ListView.separated(
+        itemCount: contact.length,
+        itemBuilder: (BuildContext context, int index) {
+          Contact _contact = contact[index];
+          return ListTile(
+            leading: CircleAvatar(),
+            title: Text(_contact.name),
+            subtitle: Text(_contact.email),
+          );
+        },
+        separatorBuilder: (context, index) => Divider(),
+      ),
+    );
+
   }
 }
