4a20fdffd6c5771432c92fa0a67f7a6a52e4b292
fonkamloic <fonkamloic@gmail.com>
2020-01-09 04:25:17 +0100

    compose message component

--- lib/message_compose.dart
+++ lib/message_compose.dart
@@ -1,4 +1,6 @@
 import 'package:email_app/message.dart';
+import 'package:email_app/messageFormManager.dart';
+import 'package:email_app/provider.dart';
 import 'package:flutter/material.dart';
 
 class MessageCompose extends StatefulWidget {
@@ -7,81 +9,82 @@ class MessageCompose extends StatefulWidget {
 }
 
 class _MessageComposeState extends State<MessageCompose> {
-  String to = "";
-  String subject = '';
-  String body = '';
-  final key = GlobalKey<FormState>();
+
 
   @override
   Widget build(BuildContext context) {
+    MessageFormManager messageFormManager =
+        Provider.of(context).fetch(MessageFormManager);
     return Scaffold(
       appBar: AppBar(
         title: Text("Compose New Message"),
       ),
       body: SingleChildScrollView(
         child: Form(
-          key: key,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
               ListTile(
-                title: TextFormField(
-                  validator: (value){
-                    return  value.contains("@") ? null : "Must be a valid email";
+                title: StreamBuilder<String>(
+                  stream: messageFormManager.email$,
+                  builder: (context, snapshot) {
+                    return TextField(
+                      onChanged: messageFormManager.inEmail.add,
+                      decoration: InputDecoration(
+                        errorText: snapshot.error,
+                        labelText: "TO",
+                        labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    );
                   },
-                  onSaved: (value) => to = value,
-                  decoration: InputDecoration(
-                    labelText: "TO",
-                    labelStyle: TextStyle(fontWeight: FontWeight.bold),
-                  ),
                 ),
               ),
               ListTile(
-                title: TextFormField(
-                  validator: (value){
-                   if(value.length < 4){
-                     return "Subject must be more that 4 characteres";
-                   }else
-                     return null;
-                  },
-                  onSaved: (value) => subject = value,
-
-                  decoration: InputDecoration(
-                    labelText: "SUBJECT",
-                    labelStyle: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ),
+                title: StreamBuilder<String>(
+                    stream: messageFormManager.subject$,
+                    builder: (context, snapshot) {
+                      return TextField(
+                        onChanged: messageFormManager.inSubject.add,
+                        decoration: InputDecoration(
+                          errorText: snapshot.error,
+                          labelText: "SUBJECT",
+                          labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      );
+                    }),
               ),
               Divider(),
               ListTile(
-                title: TextFormField(
-                  validator: (value){
-                    if(value.length == 0){
-                      return "Body can't be empty";
-                    }
-                    else
-                      return null;
-                  },
-                  onSaved: (value) => body = value,
-                  maxLines: 5,
-                  decoration: InputDecoration(
-                    labelText: "BODY",
-                    labelStyle: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ),
+                title: StreamBuilder<String>(
+                    stream: messageFormManager.body$,
+                    builder: (context, snapshot) {
+                      return TextField(
+                        onChanged: messageFormManager.inBody.add,
+                        maxLines: 5,
+                        decoration: InputDecoration(
+                          errorText: snapshot.error,
+                          labelText: "BODY",
+                          labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      );
+                    }),
               ),
               ListTile(
-                title: RaisedButton(
-                  child: Text("SEND"),
-                  onPressed: () async{
-                    if(this.key.currentState.validate()){
-                      await this.key.currentState.save();
-                      Message message = Message(subject: subject, body: body);
-                      Navigator.pop(context, message);
-                    }
-
-                  },
-                ),
+                title: StreamBuilder<bool>(
+                    stream: messageFormManager.isFormValid$,
+                    builder: (context, snapshot) {
+                      return RaisedButton(
+                        child: Text("SEND"),
+                        onPressed: ()async {
+                          if (snapshot.hasData) {
+                            Message message = await messageFormManager.submit();
+                            Navigator.pop(context, message);
+                            print("yes");
+                          } else
+                            print(snapshot.data);
+                        },
+                      );
+                    }),
               ),
             ],
           ),
