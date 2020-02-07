61142c35fea09af05bf78f4b4e5b32c7f632329c
fonkamloic <fonkamloic@gmail.com>
2020-02-08 00:31:34 +0100

    counter component

--- lib/counter.dart
+++ lib/counter.dart
@@ -1,12 +1,12 @@
 import 'package:email_app/managers/counterManager.dart';
-import 'package:email_app/observer.dart';
-import 'package:email_app/provider.dart';
 import 'package:flutter/material.dart';
+import 'package:sprinkle/Observer.dart';
+import 'package:sprinkle/SprinkleExtension.dart';
 
 class Counter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    CounterManager manager = Provider.of(context).fetch(CounterManager);
+    CounterManager manager = context.fetch<CounterManager>();
     return Center(
       child: Observer<int>(
         stream: manager.counter$,
