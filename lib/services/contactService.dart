7d19bd76f9ad5a4a20dacc5489e25eb942fa1647
fonkamloic <fonkamloic@gmail.com>
2020-02-08 00:32:29 +0100

    contact services

--- lib/services/contactService.dart
+++ lib/services/contactService.dart
@@ -2,20 +2,24 @@ import 'dart:convert';
 
 import 'package:email_app/model/contact.dart';
 import 'package:http/http.dart' as http;
+import 'package:sprinkle/Service.dart';
 
-class ContactService {
-  static String _url = "https://jsonplaceholder.typicode.com/users";
+class ContactService implements Service<Contact>{
+  String _url = "http://fip.zaiste.net/contacts";
 
-  static Future browse({query}) async {
-    http.Response response = await http.get(_url);
+  Future<List<Contact>> browse({String filter}) async {
+    http.Response response = await http.get("$_url?q=$filter");
+
+
     String content = response.body;
+
+    await Future.delayed(Duration(seconds: 1));
+
+
     List collection = json.decode(content);
+
     Iterable<Contact> _contacts =
         collection.map((json) => Contact.fromJson(json));
-    if (query != null && query.isNotEmpty) {
-      _contacts = _contacts
-          .where((contact) => contact.name.toLowerCase().contains(query));
-    }
     return _contacts.toList();
   }
 }
