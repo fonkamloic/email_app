17fc469fae9b65039c18a5d93850503ada6fb05c
fonkamloic <fonkamloic@gmail.com>
2020-02-08 00:32:49 +0100

    web resource manager

--- lib/managers/WebResourceManager.dart
+++ lib/managers/WebResourceManager.dart
@@ -1,31 +1,29 @@
 import 'dart:async';
 
 import 'package:email_app/manager.dart';
-import 'package:email_app/model/contact.dart';
 import 'package:rxdart/rxdart.dart';
+import 'package:sprinkle/Service.dart';
 
-abstract class Service<T> {
-  Future<List<T>> browse({String filter});
-}
+
 
 class WebResourceManager<T> implements Manager {
   final PublishSubject<String> _filterSubject = PublishSubject<String>();
   final BehaviorSubject<int> _countSubject = BehaviorSubject<int>();
-  final BehaviorSubject<List<Contact>> _collectionSubject = BehaviorSubject();
+  final BehaviorSubject<List<T>> _collectionSubject = BehaviorSubject();
 
   Sink<String> get inFilter => _filterSubject.sink;
 
   Stream<int> get count$ => _countSubject.stream;
 
-  Stream<List<Contact>> get browse$ => _collectionSubject.stream;
+  Stream<List<T>> get collection$ => _collectionSubject.stream;
 
-  WebResourceManager(Service service) {
+  WebResourceManager(Service<T> service) {
     _filterSubject
         .debounceTime(Duration(milliseconds: 500))
         .switchMap((filter) async* {
       yield await service.browse(filter: filter);
-    }).listen((contacts) async {
-      _collectionSubject.add(contacts);
+    }).listen((elements) async {
+      _collectionSubject.add(elements);
     });
 
     _collectionSubject.listen((list) => _countSubject.add(list.length));
@@ -37,4 +35,3 @@ class WebResourceManager<T> implements Manager {
     _collectionSubject.close();
   }
 }
-
